d215c26302164d2d5c2b4551e8504f5f28ec5220
Dinil Thomas <dinilvallikunnil@gmail.com>
2023-12-06 23:14:11 +0530

    budgetCalculator

--- script.js
+++ script.js
@@ -113,7 +113,7 @@ function logout(){
 
 function enterBudget(){
     const budget = document.getElementById("budget").value;
-    inner1.innerHTML =`<div class="budg">
+    inner1.innerHTML =`<div class="budg ">
                             <p>${budget}</p>
                         <div>`
     
@@ -137,11 +137,11 @@ function enterData(date='N/A',event='N/A', amount='N/A') {
     else{
         // let inputs = [date,event,amount];
       exp = Number(exp)+Number(amount);
-      inner2.innerHTML =`<div class="bu">
+      inner2.innerHTML =`<div class="bu col-lg-10 col-lg-10 col-xs-10">
       <p>${exp}</p>
       <div>`
       bala = Number(budget)-exp;
-      inner3.innerHTML =`<div class="bu">
+      inner3.innerHTML =`<div class="bu col-lg-10 col-lg-10 col-xs-10">
       <p>${bala}</p>
       <div>`
       tot.innerHTML = `<div>Total Amount: ${exp}</div>`
